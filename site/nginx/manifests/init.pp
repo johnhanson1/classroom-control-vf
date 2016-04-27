31a11c665986e04da9b60ca80f7773b2a020c8cc
johnhanson1 <john.hanson1@tr.com>
2016-04-27 16:54:02 +0100

    nginx

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,1 +1,34 @@
-include nginx
+class nginx {
+  package {'nginx':
+  ensure => present,
+  }
+  
+  file {'/var/www':
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0775',
+  }
+  
+  file {'/var/www/index.html':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
+    source => 'puppet:///modules/nginx/index.html',
+  }    
+    
+  file {'/etc/nginx/nginx.conf':
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
+    source => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
+    notifiy => Service['nginx'],
+  }
+    
+  service {'nginx':
+    ensure => running,
+    enable => true,
+  }
+}
