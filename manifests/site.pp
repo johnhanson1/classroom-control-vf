f437110fb21a9843f60a7a57e34f860ae612ef7a
johnhanson1 <john.hanson1@tr.com>
2016-04-28 15:09:37 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,8 +42,11 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  if $::virtual != 'physical' {
-    $vmname = capitalize($::virtual)
-    notify { "Hello, my name is ${::hostname} - This is a ${vmname} virtual machine.":}
-  }
+  $message = hiera('message')
+    notify { $message: }
+  
+#  if $::virtual != 'physical' {
+#    $vmname = capitalize($::virtual)
+#    notify { "Hello, my name is ${::hostname} - This is a ${vmname} virtual machine.":}
+#  }
 }
